34ba7d75e40139af6c381a309ac7c60c1389da0e
zhangquan <quanys09@qq.com>
2018-07-15 22:44:11 +0800

    修改localhost

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@ export default {
     display: flex;
     height: 40px;
     line-height: 40px;
-    border-bottom: 1px solid gray;
+    border-bottom: 1px solid rgba(7, 17, 27, 0.1);
     .tab-item {
       flex: 1;
       text-align: center;
